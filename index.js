452c4444d88b78c32f99ecc8762b572524192753
Janari Altdorf <janarialtdorf@users.noreply.github.com>
2025-10-04 19:36:59 +0300

    Kõik mis külastuste registreerimise kodutöös tegema pidi

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const fs = require ("fs");
 //päringu lahtiharutaja POST jaoks:
 const bodyparser = require("body-parser");
 const textRef = "public/txt/vanasonad.txt";
+const logRef = "public/txt/visitlog.txt";
 //käivitan express.js funktsiooni ja annan talle nimeks "app".
 const app = express();
 //Määran veebilehtede mallide renderdamise mooduli ehk VIEW ENGINE
@@ -21,7 +22,8 @@ app.get("/", (req, res)=>{
 app.get("/timenow", (req, res)=>{
 	const weekDayNow = dateET.weekDay();
 	const dateNow = dateET.fullDate();
-	res.render("timenow", {weekDayNow: weekDayNow, dateNow: dateNow});
+	const fullTime = dateET.fullTime();
+	res.render("timenow", {weekDayNow: weekDayNow, dateNow: dateNow, timeNow: fullTime});
 });
 
 app.get("/vanasonad", (req, res)=>{
@@ -41,6 +43,10 @@ app.get("/regvisit", (req, res)=>{
 	res.render("regvisit");
 });
 
+app.get("/kulastus", (req, res)=>{
+	res.render("visitregistered");
+});
+
 app.post("/regvisit", (req, res)=>{
 	console.log(req.body);
 	//avan tekstifaili kirjutamiseks sellisel moel, et kui teda pole, siis ta luuakse:
@@ -50,13 +56,13 @@ app.post("/regvisit", (req, res)=>{
 		}
 		else{
 			//faili senisele sisule lisamine
-		fs.appendFile("public/txt/visitlog.txt", req.body.nameInput + "; ", (err)=>{
+		fs.appendFile("public/txt/visitlog.txt", req.body.firstNameInput + " " + req.body.lastNameInput + " " + dateET.fullDate() + " " + "kell: " + dateET.fullTime() +  "; ", (err)=>{
 			if(err){
 				throw(err);
 			}
 			else{
 				console.log("Salvestatud!");
-				res.render("regvisit");
+				res.render("visitregistered");
 			}
 		});
 		}
@@ -64,6 +70,17 @@ app.post("/regvisit", (req, res)=>{
 	
 });
 
-
+app.get("/visitlog", (req, res)=>{
+	let visitLog = [];
+	fs.readFile(logRef, "utf8", (err, data)=>{
+		if(err){
+			res.render("visitlog", {heading: "Külastuste nimekiri", listData: ["Nimekiri puudub!"]});
+		}
+		else{
+			visitLog = data.split(";");
+			res.render("visitlog", {heading: "Külastuste nimekiri", listData: visitLog});
+		}
+	});
+});
 
 app.listen(5101);
